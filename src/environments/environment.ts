0e87342eca4279df1f83ef1e610c9bf3f1f651d8
kasyifana <muhammadli1009@gmail.com>
2025-12-02 08:54:01 +0700

    Fix: Add fileReplacements for prod build & update dev env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: '/api/v1',   // ← PENTING: TANPA http://103.49...
+  apiBaseUrl: 'http://103.49.239.164:3000/api/v1',
   fileBaseUrl: 'http://103.49.239.164:3000',
 };
